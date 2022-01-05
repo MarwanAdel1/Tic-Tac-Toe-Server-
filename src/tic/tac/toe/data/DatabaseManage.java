5acd267cfdd1ce95032a8b363795e67696efd457
Zeinab <zeinabibrahim541@gmail.com>
2022-01-05 13:15:38 +0200

    update Server

---
+++ src/tic/tac/toe/data/DatabaseManage.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tic.tac.toe.data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.derby.jdbc.ClientDriver;
+
+
+public class DatabaseManage {
+    public static Connection connection;
+    public static Connection getConn(){
+        if(connection ==null){
+        try {
+            DriverManager.registerDriver(new ClientDriver());
+             connection=DriverManager.getConnection("jdbc:derby://localhost:1527/PhoneBook","root","root");
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseManage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        }
+        return connection;
+    }
+    
+   
+    
+}
