b5a92187fb9e4c1dcd19c777ba53446ca14bea8c
nour gweda <nourgweda1@gmail.com>
2022-01-06 14:14:06 +0200

    server json update

--- src/ui/TicTacToeServer.java
+++ src/ui/TicTacToeServer.java
@@ -9,7 +9,7 @@ import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.apache.derby.jdbc.ClientDriver;
+//import org.apache.derby.jdbc.ClientDriver;
 
 /**
  *
